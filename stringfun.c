f4bbd43191ebf4ba5907e0c92e9d3b8c68a3cd02
DJ <johndaya@mail.gvsu.edu>
2021-12-14 02:44:12 -0500

    final exam file

---
+++ stringfun.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void){
+size_t maxn = 50;
+char *word;
+word = (char *) malloc(sizeof(char)*maxn);
+
+getline(&word, &maxn, stdin);
+for(int i = strlen(word) - 1; i >= 0; i--){
+	if(word[i] == 'g' || word[i] == 'v' || word[i] == 's' || word[i] == 'u'){
+
+	}
+	else{
+		printf("%c", word[i]);
+	}
+}
+printf("\n");
+
+
+free(word);
+
+return 0;
+}
